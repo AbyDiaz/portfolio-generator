7954943dc003e1e823aa4829d1d918c7ace1657d
abbyd <abbydiaz300@gmail.com>
2022-04-29 20:06:28 -0700

    seeing if commit is needed for node app to work

--- app.js
+++ app.js
@@ -1,2 +1,16 @@
 const profileDataArgs = process.argv.slice(2);
 
+//const generatePage = (userName, githubName) => {
+  //  return `
+    //Name: ${userName}
+    //GitHub: ${githubName}
+    //`;
+//};
+
+const generatePage = (userName, githubName) => {
+    return `
+      Name: ${userName}
+      GitHub: ${githubName}
+    `;
+  };
+  
