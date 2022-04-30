5f62b9e0cbbfacfb47bdf95cf56d6a43cd0996ec
abbyd <abbydiaz300@gmail.com>
2022-04-29 20:11:10 -0700

    node app not showing hope this will work

--- app.js
+++ app.js
@@ -1,12 +1,5 @@
 const profileDataArgs = process.argv.slice(2);
 
-//const generatePage = (userName, githubName) => {
-  //  return `
-    //Name: ${userName}
-    //GitHub: ${githubName}
-    //`;
-//};
-
 const generatePage = (userName, githubName) => {
     return `
       Name: ${userName}
