60d016049c4e67188695d78f1c0aa38ff807c6c4
abbyd <abbydiaz300@gmail.com>
2022-05-05 19:03:22 -0700

    added new file for the page template to pull html from

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
-
-const generatePage = (userName, githubName) => {
-    return `
-      Name: ${userName}
-      GitHub: ${githubName}
-    `;
-  };
-  console.log(name, github);
-  console.log(generatePage(name, github));
   
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if (err) throw new Error(err);
 
+    console.log('Portfolio complete! Check out index.html to see the output!');
+
+});
